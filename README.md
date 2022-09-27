6fd13a56e6510712bdc0776b0065231e10772839
Jasmeet Singh <jasmeet0915@gmail.com>
2022-09-27 22:02:55 +0530

    Added turtlebot3 pkgs

---
+++ README.md
@@ -0,0 +1,12 @@
+# Testing Model Views
+
+> `env_hooks` won't work with `zsh`
+
+## Requirements
+ * kobuki_description
+
+    roslaunch turtlebot_rviz_launchers view_model.launch
+
+It is a standalone launcher with minimal dependencies. Comments
+on how to reconfigure your environment to view the different
+robot models are in the launcher.
--- turtlebotcity_gazebo/env_hooks/turtlebot_city_gazebo.bash
+++ turtlebotcity_gazebo/env_hooks/turtlebot_city_gazebo.bash
@@ -3,3 +3,4 @@
 source /usr/share/gazebo/setup.bash
 export GAZEBO_MODEL_PATH="${CATKIN_ENV_HOOK_WORKSPACE}/../src/turtlebotcity-rigbetellabs/turtlebotcity_gazebo/models/:${GAZEBO_MODEL_PATH}"
 export GAZEBO_RESOURCE_PATH="${CATKIN_ENV_HOOK_WORKSPACE}/../src/turtlebotcity-rigbetellabs/turtlebotcity_gazebo/models/:${CATKIN_ENV_HOOK_WORKSPACE}/../src/turtlebotcity-rigbetellabs/turtlebotcity_gazebo/worlds/:${GAZEBO_RESOURCE_PATH}"
+export TURTLEBOT3_MODEL=burger
